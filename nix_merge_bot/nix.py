008ef18580fce2ca281a841f65dbb4077065c9b2
lassulus <git@lassul.us>
2023-10-13 23:47:11 +0200

    init nix.py

---
+++ nix_merge_bot/nix.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import subprocess
+import json
+
+from .github import http_request
+
+class MergeResponse:
+    def __init__(self, permitted: bool, meta: dict):
+        self.permitted = permitted
+        self.meta = meta
+
+def get_package_maintainers(path: str) -> list[str]:
+    path_ = Path(path)
+    proc = subprocess.run(
+        [
+            "nix",
+            "eval",
+            "--refresh",
+            "--json",
+            f"github:nixos/nixpkgs/master#{path_.parent.name}.meta.maintainers"
+        ],
+        check=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    maintainers = json.loads(proc.stdout.decode("utf-8"))
+    return [ maintainer["githubId"] for maintainer in maintainers ]
+
+
+def merge_pr(pr_number: int, github_id: int):
+    files_response = http_request(f"https://api.github.com/repos/nixos/nixpkgs/pulls/{pr_number}/files")
+    meta = {}
+    permitted = True
+    for file in files_response.json():
+        filename = file["filename"]
+        if not filename.startswith("pkgs/by-name/"):
+            permitted = False
+            meta[filename] = "path is not in pkgs/by-name/"
+        else:
+            maintainers = get_package_maintainers(filename)
+            if github_id not in maintainers:
+                permitted = False
+                meta[filename] = "github id is not in maintainers, valid maintainers are: " + ", ".join(maintainers)
+    return MergeResponse(permitted, meta)
