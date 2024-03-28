4a1b6982e55cef103a9ec16a86d32bbc1266fff9
Scriptkiddi <fritz@otlinghaus.it>
2024-03-28 10:35:11 +0100

    Fix: use correct webhook event for review comments

--- nixpkgs_merge_bot/github/PullRequest.py
+++ nixpkgs_merge_bot/github/PullRequest.py
@@ -1,5 +1,8 @@
+import logging
 from dataclasses import dataclass
 from typing import Any
+
+log = logging.getLogger(__name__)
 
 
 @dataclass
@@ -17,18 +20,23 @@ class PullRequest:
 
     @staticmethod
     def from_json(body: dict[str, Any]) -> "PullRequest":
-        return PullRequest(
-            user_id=body["user"]["id"],
-            user_login=body["user"]["login"],
-            text=body["body"],
-            repo_owner=body["base"]["repo"]["owner"]["login"],
-            repo_name=body["base"]["repo"]["name"],
-            number=body["number"],
-            title=body["title"],
-            state=body["state"],
-            head_sha=body["head"]["sha"],
-            ref=body["base"]["ref"],
-        )
+        try:
+            return PullRequest(
+                user_id=body["user"]["id"],
+                user_login=body["user"]["login"],
+                text=body["body"],
+                repo_owner=body["base"]["repo"]["owner"]["login"],
+                repo_name=body["base"]["repo"]["name"],
+                number=body["number"],
+                title=body["title"],
+                state=body["state"],
+                head_sha=body["head"]["sha"],
+                ref=body["base"]["ref"],
+            )
+        except KeyError as e:
+            log.debug(e)
+            log.debug(body)
+            raise e
 
     def __str__(self) -> str:
         return f"Pull Request:  {self.title} by {self.user_login} with head sha {self.head_sha} "
