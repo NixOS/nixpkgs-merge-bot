b2e8cf0cd1b916a4d27e914855143ddaf37f7825
Jörg Thalheim <joerg@thalheim.io>
2024-12-01 14:29:42 +0100

    treefmt: remove unused args and silent some warnings

--- nix/treefmt/flake-module.nix
+++ nix/treefmt/flake-module.nix
@@ -3,27 +3,26 @@
   imports = [
     inputs.treefmt-nix.flakeModule
   ];
-  perSystem =
-    { pkgs, ... }:
-    {
-      treefmt = {
-        projectRootFile = ".git/config";
-        programs.nixfmt.enable = true;
-        programs.shellcheck.enable = true;
-        programs.deno.enable = true;
-        settings.formatter.deno.excludes = [
-          "tests/data/*.json"
-          "secrets.yaml"
-        ];
-        settings.formatter.shellcheck.options = [
-          "-s"
-          "bash"
-        ];
+  perSystem = {
+    treefmt = {
+      projectRootFile = ".git/config";
+      programs.nixfmt.enable = true;
+      programs.shellcheck.enable = true;
+      programs.deno.enable = true;
 
-        programs.mypy.enable = true;
-        programs.mypy.directories."." = { };
-        programs.ruff.check = true;
-        programs.ruff.format = true;
-      };
+      settings.global.excludes = [
+        "tests/data/*.json"
+        "secrets.yaml"
+      ];
+      settings.formatter.shellcheck.options = [
+        "-s"
+        "bash"
+      ];
+
+      programs.mypy.enable = true;
+      programs.mypy.directories."." = { };
+      programs.ruff.check = true;
+      programs.ruff.format = true;
     };
+  };
 }
